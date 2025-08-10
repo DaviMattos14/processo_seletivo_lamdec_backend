ba49073220b98c7e2a5b76974afe0830aff8dfb2
Davi Mattos <contato.davimattos14@gmail.com>
2025-08-10 16:13:24 -0300

    Banco de Dados Relacional Criado

---
+++ Modelos/Modelo_Fisico.sql
@@ -0,0 +1,62 @@
+DROP DATABASE cda;
+CREATE DATABASE cda;
+USE cda;
+CREATE TABLE Natureza_da_Divida (
+    idNaturezaDivida INTEGER PRIMARY KEY,
+    nomeNaturezaDivida VARCHAR(255),
+    descricao VARCHAR(255)
+);
+
+CREATE TABLE Situacao (
+    codSituacao INTEGER PRIMARY KEY,
+    nomeSituacao VARCHAR(255),
+    codSituacaoFiscal INTEGER,
+    codExigibilidade INTEGER,
+    tipo VARCHAR(255)
+);
+
+CREATE TABLE Divida_Ativa (
+    numCDA VARCHAR(255),
+    anoInscricao INTEGER,
+    valSaldo DECIMAL(18, 2),
+    datCadastramento DATE,
+    idNaturezaDivida INTEGER,
+    codSituacao INTEGER,
+    datSituacao DATE,
+    horaSituacao TIME,
+    codFaseCobranca INTEGER,
+    probabilidade_Recuperacao FLOAT,
+    status_Recuperacao INTEGER,
+    CONSTRAINT PK_Divida_Ativa PRIMARY KEY (numCDA, datSituacao, horaSituacao),
+    CONSTRAINT FK_Divida_Natureza FOREIGN KEY (idNaturezaDivida)
+        REFERENCES Natureza_da_Divida (idNaturezaDivida)
+        ON DELETE RESTRICT,
+    CONSTRAINT FK_Divida_Situacao FOREIGN KEY (codSituacao)
+        REFERENCES Situacao (codSituacao)
+        ON DELETE RESTRICT
+);
+
+CREATE TABLE Pessoa (
+    idPessoa INTEGER PRIMARY KEY,
+    nomePessoa VARCHAR(255),
+    cnpj VARCHAR(14), 
+    cpf VARCHAR(11) UNIQUE,  
+    Pessoa_TIPO VARCHAR(255)  
+);
+
+
+CREATE TABLE Situacao_Devedor (
+    idPessoa INTEGER,
+    numCDA VARCHAR(255),
+    descSituacaoDevedor VARCHAR(255),
+    CONSTRAINT PK_Situacao_Devedor PRIMARY KEY (numCDA, descSituacaoDevedor, idPessoa), 
+    CONSTRAINT FK_Situacao_Pessoa FOREIGN KEY (idPessoa)
+        REFERENCES Pessoa (idPessoa)
+        ON DELETE CASCADE,
+    CONSTRAINT FK_Situacao_Divida FOREIGN KEY (numCDA)
+        REFERENCES Divida_Ativa (numCDA)
+        ON DELETE CASCADE
+);
+
+INSERT INTO Pessoa (idpessoa, nomePessoa, Pessoa_TIPO)
+VALUES (-1, 'Pessoa Não Identificada', 'DESCONHECIDO');
---
+++ connect_bd.py
@@ -0,0 +1,64 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
+import pandas as pd
+import os
+
+def connect_mysql(bd_name, user, password):
+    """
+    Função para conectar ao banco de dados MySQL usando SQLAlchemy.
+    """
+    engine = None
+    try:
+        usuario = user
+        senha = password
+        host = 'localhost'
+        porta = '3306'
+        banco = bd_name
+
+        # Criação da engine de conexão
+        engine = create_engine(f'mysql+pymysql://{usuario}:{senha}@{host}:{porta}/{banco}')
+        
+        # Testar a conexão abrindo uma conexão real
+        with engine.connect() as conn:
+            print("Conexão com o MySQL via SQLAlchemy bem-sucedida!")
+
+    except SQLAlchemyError as e:
+        print(f"Erro ao conectar com o MySQL via SQLAlchemy: {e}")
+    
+    return engine
+
+def execute_query(engine, query: str):
+    """
+    Executa uma query de escrita (INSERT, UPDATE, DELETE, DDL) usando SQLAlchemy.
+    """
+    try:
+        with engine.begin() as conn: 
+            conn.execute(text(query))
+            print("Query executada com sucesso!")
+    except SQLAlchemyError as e:
+        print(f"Erro ao executar a query: {e}")
+
+def read_query(engine, query):
+    """
+    Função para executar uma query SELECT usando SQLAlchemy e retornar um DataFrame.
+    """
+    try:
+        # Lê a query diretamente como DataFrame
+        df = pd.read_sql(query, con=engine)
+        return df
+    except SQLAlchemyError as err:
+        print(f"Erro ao executar a query: {err}")
+        return None
+    
+def create_database(filepath, user, password):
+    usuario = user
+    senha = password
+    host = 'localhost'
+    porta = '3306'
+    engine = create_engine(f'mysql+pymysql://{usuario}:{senha}@{host}:{porta}')
+    
+    with open(filepath, 'r') as file:
+        command = file.read().strip().split(';')
+        for linha in command:
+            execute_query(engine,linha)
+    print("Sucesso")
